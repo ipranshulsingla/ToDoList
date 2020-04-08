30f2a6a7ce05156d0047e427b4d37e331b7ccfc4
ipranshulsingla <52979328+ipranshulsingla@users.noreply.github.com>
2020-04-08 21:42:10 +0530

    Add files via upload

---
+++ precache-manifest.d89f712fbd796b6334b91bb2fedd284d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8fa3e5c948a6a359f8bbc2f3116a33",
+    "url": "https://github.com/ipranshulsingla/ToDoList/index.html"
+  },
+  {
+    "revision": "d288d888327e6b68dee7",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0ff995a5a3f2b72bc083",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/css/main.7b620376.chunk.css"
+  },
+  {
+    "revision": "d288d888327e6b68dee7",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/js/2.028b72d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/js/2.028b72d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff995a5a3f2b72bc083",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/js/main.4bf01422.chunk.js"
+  },
+  {
+    "revision": "38fbe8e51b6db561195b",
+    "url": "https://github.com/ipranshulsingla/ToDoList/static/js/runtime-main.780f94b0.js"
+  }
+]);
